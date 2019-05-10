d481435f0f4df6accdec5b0176c196667cccee12
jriggs28 <jriggs@live.ca>
2019-05-09 20:21:20 -0700

    Add files via upload
    Base

---
+++ h-run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+
+
+#installing libuv1 if necessary
+[[ `dpkg -s libuv1 2>/dev/null | grep -c "ok installed"` -eq 0 ]] && apt-get install -y libuv1
+
+#Ubuntu 18.04 compat
+[[ -e /usr/lib/x86_64-linux-gnu/libcurl-compat.so.3.0.0 ]] &&
+	export LD_PRELOAD=libcurl-compat.so.3.0.0
+
+[ -t 1 ] && . colors
+
+. h-manifest.conf
+
+[[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
+[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
+[[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
+CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
+[[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
+
+./miner $(< /hive/miners/custom/$CUSTOM_NAME/$CUSTOM_NAME.conf) $@ 2>&1 | tee $CUSTOM_LOG_BASENAME.log
---
+++ h-stats.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+stats_raw=`curl --connect-timeout 2 --max-time 5 --silent --noproxy '*' http://127.0.0.1:60050`
+
+khs=`echo $stats_raw | jq -r '.hashrate.total[0]'`
+stats=`echo $stats_raw | jq '{hs: [.hashrate.threads[][0]], hs_units: "khs", uptime: .connection.uptime, ar: [.results.shares_good, .results.shares_total - .results.shares_good], algo: "wildkeccak"}'`
+
+#echo $khs
+#echo $stats
